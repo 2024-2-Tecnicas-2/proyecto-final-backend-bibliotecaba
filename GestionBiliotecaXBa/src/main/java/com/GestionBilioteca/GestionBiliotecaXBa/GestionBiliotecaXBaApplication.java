fdf8d83ebec3d678a2afcca58a1d6f7862ac13eb
blancoblanco <pentagonocuadrado2@gmail.com>
2024-10-03 23:22:19 -0500

    proyecto java

---
+++ GestionBiliotecaXBa/src/main/java/com/GestionBilioteca/GestionBiliotecaXBa/GestionBiliotecaXBaApplication.java
@@ -0,0 +1,13 @@
+package com.GestionBilioteca.GestionBiliotecaXBa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBiliotecaXBaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBiliotecaXBaApplication.class, args);
+	}
+
+}
---
+++ GestionBiliotecaXBa/src/test/java/com/GestionBilioteca/GestionBiliotecaXBa/GestionBiliotecaXBaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionBilioteca.GestionBiliotecaXBa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBiliotecaXBaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
